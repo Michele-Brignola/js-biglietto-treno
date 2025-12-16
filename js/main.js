4cdb701a258b8e97522ef3a6cd9efd82f0ce6efb
Michele-Brignola <michele.brignola@hotmail.com>
2025-12-16 16:03:36 +0100

    added bonus

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-const chilometriDaPercorrere = parseFloat(prompt("Quanti chilometri vuoi percorrere?"));
-const etaUtente = parseInt(prompt("Quanti anni hai?"));
+const chilometriDaPercorrere = parseFloat(prompt(`Quanti chilometri vuoi percorrere?`));
+const etaUtente = parseInt(prompt(`Quanti anni hai?`));
+const nomeUtente = prompt(`Come ti chiami?`)
 
-costoViaggio(chilometriDaPercorrere, etaUtente);
+if (isNaN(chilometriDaPercorrere) || isNaN(etaUtente) || isNaN(nomeUtente)) {
+  console.log(`I valori non sono validi`);
+  alert(`I valori non sono validi`);
+}
+else {
+  costoViaggio(chilometriDaPercorrere, etaUtente, nomeUtente);
+}
 
-function costoViaggio(disChilometri, eta) {
+function costoViaggio(disChilometri, eta, nome) {
   let costo = 0.21 * disChilometri;
   
+  if (nome.toLowerCase() === `arthur`) {
+    costo = 0
+  }
+  else if (nome.toLowerCase() === `tiziano`) {
+    costo = 0.5 * disChilometri;
+  }
+
   if (eta < 18) {
     costo = costo * 0.8;
   } 
